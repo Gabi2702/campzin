cfa23373d9f121c81da51db2a028e5092daaf325
Gabriel <gabrielbasi_@hotmail.com>
2024-10-11 00:33:39 -0300

    Fix images card games

--- src/components/card/cardGames.js
+++ src/components/card/cardGames.js
@@ -6,31 +6,30 @@ export default function CardGames({ InfoGames, size = "lg" }) {
 
   const types = {
     "lg" : {
-      container: "grid grid-cols-5 gap-4 p-4",
+      container: "grid grid-cols-5 gap-4 p-4 mb-2",
       card_link : "w-[300px] h-[300px] hover:cursor-pointer",
-      img_size : "w-full h-[90%] p-2 object-cover rounded-2xl transition-transform duration-300 ease-in-out transform hover:scale-105", // Adicionado efeito de hover
+      img_size : "w-full h-[92%] p-2 object-cover rounded-2xl transition-transform duration-300 ease-in-out transform hover:scale-105", 
       text_game_name : true
     },
     "sm" : {
-      container: "flex flex-row gap-2 ",
+      container: "flex flex-row gap-2 mb-2",
       card_link : "w-18 h-18 hover:cursor-pointer -mt-4",
-      img_size : "w-[80px] h-[80px] object-cover rounded-md transition-transform duration-300 ease-in-out transform hover:scale-105", // Adicionado efeito de hover
+      img_size : "w-[80px] h-[80px] object-cover rounded-md transition-transform duration-300 ease-in-out transform hover:scale-105 p-2", 
       text_game_name : false
     }
   }
 
   return (
     <div className={types[size].container}>
-      {Array.from({ length: InfoGames?.length }, (_, index) => (
-        <CardWithLink key={index} className={types[size].card_link} urlLink={`/tournaments/game/${InfoGames[index].search_query}`}>
+      {InfoGames?.map((game, index) => (
+        <CardWithLink key={index} className={types[size].card_link} urlLink={`/tournaments/game/${game.search_query}`}>
           <img
-            src={InfoGames[index].image || "https://static-esports.ubisoft.com/esports-platform/r6s/championship/0079620d-4622-4443-9d8a-31cdc889309c.png"}
+            src={game[`image_${size}`]} 
             alt={`Image ${index + 1}`}
             className={types[size].img_size}
           />
-          { types[size].text_game_name && 
-          (
-            <p className='font-bold text-sm text-center text-white'>{InfoGames[index].name}</p>
+          {types[size].text_game_name && (
+            <p className="text-center">{game.name}</p>
           )}
         </CardWithLink>
       ))}
